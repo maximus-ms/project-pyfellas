f2047db8da73a65ff2e5796af5c91e3731ea69be
Anti4ris98 <142513292+Anti4ris98@users.noreply.github.com>
2023-12-20 21:50:49 +0200

    Load and save func

--- Book.py
+++ Book.py
@@ -1,6 +1,6 @@
 from BaseClasses import *
 from Contacts import Contacts
-
+import pickle
 
 # from Notes import Notes
 class Notes(CmdProvider):
@@ -21,9 +21,13 @@ class Book:
         self.load_from_file()
 
     def load_from_file(self):
-        # TODO
+        with open(self.filename, "rb") as f:
+            data = pickle.load(f)
+            self.contact = data.get("contacts", {})
+            self.notes = data.get("notes", {})
         pass
 
     def save_to_file(self):
-        # TODO
-        pass
+        data = {"contacts": self.contacts, "notes": self.notes}
+        with open(self.filename, "wb") as f:
+                pickle.dump(data, f)
